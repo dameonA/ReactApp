2388b0282169cf0c8b6688085b856fc39e613b73
dameonA <dameon.adolphin@gmail.com>
2020-11-04 12:55:41 -0800

    Initial Start to React App

---
+++ at/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Grocery Shopping App
+        <ul>
+          <li>Bananas</li>
+          <li>Eggs</li>
+          <li>Kiwi</li>          
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
